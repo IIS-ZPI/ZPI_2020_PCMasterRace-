850889795731061498d5ddb70d1dc15e4db7cb14
Mateusz Łukomski <187974@edu.p.lodz.pl>
2020-03-27 18:59:07 +0100

    commments added

--- Calculator/src/ArithmeticsDiff.java
+++ Calculator/src/ArithmeticsDiff.java
@@ -1,4 +1,4 @@
-public class ArithmeticsDiff implements IArithmeticsDiff {
+public class ArithmeticsDiff implements IArithmeticsDiff {//komentarz MateuszL2
 
     @Override public double difference(double A, double B) {
         return A-B;
