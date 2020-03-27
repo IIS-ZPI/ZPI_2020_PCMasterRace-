a105b059a73950b4a36a1b9b696d6341c3c46dc7
Mateusz Łukomski <187974@edu.p.lodz.pl>
2020-03-27 18:27:50 +0100

    rename Class

--- Calculator/src/ArithmeticsDiffImplementation.java
+++ Calculator/src/ArithmeticsDiffImplementation.java
@@ -1,4 +1,4 @@
-public class IArithmeticsDiffImplementation implements IArithmeticsDiff{
+public class ArithmeticsDiffImplementation implements IArithmeticsDiff{
 
     @Override public double difference(double A, double B) {
         return A-B;
