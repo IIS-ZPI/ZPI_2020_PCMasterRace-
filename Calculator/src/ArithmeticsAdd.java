3f0b481a5b5eabe71721ca847677a48c8b773fbb
Mateusz Łukomski <187974@edu.p.lodz.pl>
2020-03-27 19:47:05 +0100

    add 3 comment

--- Calculator/src/ArithmeticsAdd.java
+++ Calculator/src/ArithmeticsAdd.java
@@ -1,4 +1,4 @@
-public class ArithmeticsAdd implements IArithmeticsAdd{
+public class ArithmeticsAdd implements IArithmeticsAdd{//mateuszL1
     @Override
     public double Addition(double A, double B) {
         return A+B;
--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,10 +1,11 @@
 public class Calculator {
 
     public static void main(String[] args){
-        System.out.println("Bartek Górkiewicz/Smirnoffq - OTL"); // krzysiek3
+        System.out.println("Bartek Górkiewicz/Smirnoffq - OTL");
         System.out.println("Mateusz Lukomski/matlukomski94 - Developer");
         System.out.println("Mateusz Rosiak/215866 - Tester");
-        System.out.println("Thomson19"); // comment tomek3
-        System.out.println("krzysiek1105"); // bartek1
+        System.out.println("Thomson19");
+        System.out.println("krzysiek1105");
     }
+
 }
