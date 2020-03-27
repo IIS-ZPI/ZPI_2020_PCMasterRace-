419cac3416953ad2217333a69a25e6dbe0afc1bf
Krzysztof Karasiński <krzysiek1105@opoczta.pl>
2020-03-27 18:49:39 +0100

    Add comment krzysiek1

--- Calculator/src/ArithmeticsDiv.java
+++ Calculator/src/ArithmeticsDiv.java
@@ -3,6 +3,6 @@ public class ArithmeticsDiv implements IArithmeticsDiv {
     public double Division(double A, double B) throws ArithmeticException {
         if (B == 0)
             throw new ArithmeticException();
-        return A / B;
+        return A / B; // krzysiek1
     }
 }
