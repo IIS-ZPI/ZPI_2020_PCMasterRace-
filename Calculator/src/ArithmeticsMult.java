4798cbf3e78e4cccaf13c68153c4604a9452a619
rosiak.mateusz <215866@edu.p.lodz.pl>
2020-03-27 18:51:54 +0100

    Multiplication refactor.

--- Calculator/src/ArithmeticsMult.java
+++ Calculator/src/ArithmeticsMult.java
@@ -1,5 +1,6 @@
 public class ArithmeticsMult implements IArithmeticsMult {
 
+    //bardzo ładna implementacja
     @Override
     public double multiplication(double A, double B) {
         return A*B;
